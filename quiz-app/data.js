5de5668fb1ec315a8a4c930721fede1292078724
unknown <ark2784@gmail.com>
2022-01-31 16:16:38 -0600

    pgrade

---
+++ quiz-app/data.js
@@ -0,0 +1,22 @@
+const quizData = [{
+    question: 'How old is Alain?',
+    a: '10',
+    b: '20',
+    c: '37',
+    d: '40',
+    correct: 'c'
+},{
+    question: 'Which programming lenguage is the best?',
+    a: 'Python',
+    b: 'Java',
+    c: 'Javascript',
+    d: 'Php',
+    correct: 'c'
+},{
+    question: 'Who is the president of the US',
+    a: 'Donald Mc Donald',
+    b: 'Biden',
+    c: 'Putin',
+    d: 'All of them',
+    correct: 'b'
+} ]
